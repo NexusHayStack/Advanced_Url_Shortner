63cc8f7474b7fcb8201b44791c7cfe03da41f54f
NebulaMakin <71604973+NebulaMakin@users.noreply.github.com>
2024-12-22 20:33:11 +0530

    Added the ping handler and notFound handler. Installed Swagger.

---
+++ swagger-config.js
@@ -0,0 +1,22 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0", // Specifies the OpenAPI version
+    info: {
+      title: "URL Shortner", // API title
+      description: "API Documentation for your app", // API description
+      version: "1.0.0", // API version
+    },
+    servers: [
+      {
+        url: "http://localhost:5000", // Base URL of your API
+        description: "Development Server",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"], // Path to the files containing Swagger comments
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+module.exports = swaggerDocs;
