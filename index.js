20b0a90a6d8386394c4fa764fcf9d56a2e2733b9
NebulaMakin <71604973+NebulaMakin@users.noreply.github.com>
2024-12-24 19:45:32 +0530

    Connected the API with the MongoDB Database and defined a User Schema in model directory to maintain discipline.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 var server = require('./lib/server');
+const connectDB = require('./lib/database');
 const app = express();
 
 
+// Initialize database connection
+connectDB();
 
 app.init = function(){
 
