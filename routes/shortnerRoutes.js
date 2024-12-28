c1d3b86906026907cf91de483226636719fcf0dd
NebulaMakin <71604973+NebulaMakin@users.noreply.github.com>
2024-12-29 01:52:16 +0530

    Created the endpoint to redirect the user from an alias to the longUrl using handlers.getUrlByAlias. Made sure the alias generated is unique, in handlers.shortenUrl

--- routes/shortnerRoutes.js
+++ routes/shortnerRoutes.js
@@ -8,12 +8,9 @@ import rateLimiter from "../middleware/rateLimiter.js";
 // Limit to 100 requests per 15 minutes
 const limiter = rateLimiter(100, 15 * 60);
 
-// Public Route
+// Secure Route
 router.post("/", authMiddleware, limiter, handlers.shortenUrl);
 
-/*
-// Secure Routes
 router.get('/:alias', authMiddleware, handlers.getUrlByAlias);
-*/
 
 export default router;
