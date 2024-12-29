3042025a4bdd687202920ac9de304e4df4e82818
NebulaMakin <71604973+NebulaMakin@users.noreply.github.com>
2024-12-29 07:22:41 +0530

    Updated handlers.getUrlByAlias to collect and save analytical data. Updated the User and Url Schema to incorporate said analytical data.

--- models/Url.js
+++ models/Url.js
@@ -7,6 +7,22 @@ const UrlSchema = new mongoose.Schema({
   topic: {type: String, required: false},
   shortUrl: { type: String, required: true, unique: true },
   createdAt: { type: Date, default: Date.now },
+  totalClicks: { type: Number, required: false},
+  uniqueUsers: { type: [String], required: false},
+  osType: [
+    {
+      osName: { type: String, required: false }, // Name of the OS
+      uniqueClicks: { type: Number, default: 0 }, // Count of unique clicks for this OS
+      uniqueUsers: { type: [String], required: false }, // Array of user IDs
+    },
+  ],
+  deviceType: [
+    {
+      deviceType: { type: String, required: false }, // Device Type
+      uniqueClicks: { type: Number, default: 0 }, // Count of unique clicks for this Device
+      uniqueUsers: { type: [String], required: false }, // Array of user IDs
+    },
+  ],
 });
 
 export default mongoose.model('Url', UrlSchema);
--- models/Users.js
+++ models/Users.js
@@ -6,6 +6,21 @@ const UserSchema = new mongoose.Schema({
   displayName: { type: String, required: false },              // User's display name
   createdAt: { type: Date, default: Date.now },                // Account creation date
   updatedAt: { type: Date, default: Date.now },                // Last update date
+  uniqueUsers: { type: [String], required: false},             // Number of Clicks by unique users
+  osType: [
+    {
+      osName: { type: String, required: false }, // Name of the OS
+      uniqueClicks: { type: Number, default: 0 }, // Count of unique clicks for this OS
+      uniqueUsers: { type: [String], required: false }, // Array of user IDs
+    },
+  ],
+  deviceType: [
+    {
+      deviceType: { type: String, required: false }, // Device Type
+      uniqueClicks: { type: Number, default: 0 }, // Count of unique clicks for this Device
+      uniqueUsers: { type: [String], required: false }, // Array of user IDs
+    },
+  ],
 });
 
 export default mongoose.model('User', UserSchema);
